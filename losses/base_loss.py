6f4f363a23691dc65f4ffc2f056eeb5b2ec22eb8
Rywaii99 <422809762@qq.com>
2025-01-23 10:43:30 +0800

    chore(arch): RCAN related changes

--- losses/base_loss.py
+++ losses/base_loss.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn as nn
 from torch.nn import functional as F
 
-from archs.vgg_arch import VGGFeatureExtractor
+from archs.VGG_arch import VGGFeatureExtractor
 from utils.registry import LOSS_REGISTRY
 from .loss_util import weighted_loss
 
