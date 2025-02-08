d10406862320ccd15c4239e48da9ee74fa449f32
Rywaii99 <422809762@qq.com>
2025-02-08 10:46:37 +0800

    chore(testing): update some model testing config

---
+++ bash/test_006_x2_G3080oA100oV100o3090.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#JSUB -n 2
+#JSUB -q gpu
+#JSUB -gpgpu 1
+#JSUB -m "gpu09 gpu10 gpu13 gpu14 gpu21 gpu22 gpu25 gpu26 gpu03 gpu05 gpu06 gpu07 gpu15 gpu20"
+#JSUB -e output/error.%J
+#JSUB -o output/output.%J
+source /apps/software/anaconda3/etc/profile.d/conda.sh
+conda activate py38tr1121cu116
+unset PYTHONPATH
+python test.py -opt=options/test/006_NLSN_x2.yml
+
